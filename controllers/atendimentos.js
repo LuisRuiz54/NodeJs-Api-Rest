ba5551499bfbfa168f25ac83ec3bb1f6c14601e8
Luis Ruiz <nessiuscaffe@gmail.com>
2021-08-04 00:13:26 -0300

    conectando servidor SQL, criando tabelas, banco de dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,7 +1,12 @@
-module.exports = app => {
- app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimento e esta realizando um GET'))
+const Atendimento = require('../models/atendimentos')
 
- app.post('/atendimentos', (req, res) => {
- console.log(req.body)
- res.send('você está na rota de atendimento e esta realizando um POST')})
+module.exports = app => {
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
+    })
 }
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/tabelas')
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro) {
+        console.log(erro)
+    } else {
+        console.log('conectado com sucesso')
+        
+        Tabelas.init(conexao)
+        
+        const app = customExpress()
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    }
+})
+
+
 
