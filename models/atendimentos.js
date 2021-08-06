5606f3956f676b3c4144cf92693002bb57417ddc
Luis Ruiz <nessiuscaffe@gmail.com>
2021-08-06 00:25:01 -0300

    criando metodo pach

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -68,6 +68,21 @@ class Atendimento {
 
      }
 
+     alterar(id, valores, res){
+         if(valores.data){
+             valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
+         }
+           const sql = 'UPDATE Atendimentos SET ? WHERE id=?'
+           
+           conexao.query(sql, [valores, id], (erro, resultados) => {
+               if(erro) {
+                   res.status(400).json(erro)
+               }else{
+                   res.status(200).json(resultados)
+               }
+           })
+     }
+
 }
 
 module.exports = new Atendimento 
