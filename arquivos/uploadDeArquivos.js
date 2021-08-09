7315b45c6580f2508daa2f4e3674ad31fc24817c
Luis Ruiz <nessiuscaffe@gmail.com>
2021-08-09 18:02:55 -0300

    errors e nova rota de uploadDeImagens , segurança de ext

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,11 +1,23 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => 
 {
-   const novoCaminho = `./assets/imagens/${nomeDoArquivo}`
-    fs.createReadStream(caminho)
-    .pipe( fs.createWriteStream(novoCaminho))
-    .on('finish', () => callbackImagemCriada(novoCaminho))
+   const tiposValidos = ['jpg', 'png', 'jpeg']
+   const tipo = path.extname(caminho) 
+   const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+   
+   if(tipoEhValido) {
+      const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
 
+       fs.createReadStream(caminho)
+         .pipe( fs.createWriteStream(novoCaminho))
+         .on('finish', () => callbackImagemCriada(false, novoCaminho))
+   
+    } else {   
+       const erro = 'Tipo é inválido'
+       console.log('Erro! tipo inválido!')
+       callbackImagemCriada(erro)
+    }
 }
 
